fa0a8a00c8cf6c61471dde50beea6caba559d54d
mather <mather314@gmail.com>
2016-04-17 14:05:11 +0900

    Validate URL format

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,9 @@
+require 'uri'
+
 class Bookmark < ActiveRecord::Base
   validates_uniqueness_of :url
   validates_presence_of :url, :title
+  validates_format_of :url, with: URI::regexp
   validates_numericality_of :clicks, greater_than_or_equal_to: 0
 
   def increment
